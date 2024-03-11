c2654cb193ce98e6aaf10863d5a7832a79717d62
Edson <biduzao.bidu21@gmail.com>
2024-03-11 15:01:00 -0300

    feat:  created delete user dto

---
+++ src/main/java/com/example/sku_manager/application/dtos/DeleteUserDTO.java
@@ -0,0 +1,11 @@
+package com.example.sku_manager.application.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DeleteUserDTO(
+
+
+        Integer id
+) {
+}
