5cba0b9db3d3bc956d35015cf2ef6ba60c964133
Edson <biduzao.bidu21@gmail.com>
2024-03-11 14:58:09 -0300

    feat: create user dto

---
+++ src/main/java/com/example/sku_manager/application/dtos/CreateUserUserDTO.java
@@ -0,0 +1,22 @@
+package com.example.sku_manager.application.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+
+public record CreateUserUserDTO(
+
+        Integer id,
+        @NotBlank
+        String username,
+
+        @NotBlank
+        String email,
+
+        @NotBlank
+        String password,
+
+        @NotBlank
+        String accesslevel
+){
+
+}
