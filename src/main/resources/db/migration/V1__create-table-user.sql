ff8c292fb2aec456349cedb41a96772a77de451b
Edson <biduzao.bidu21@gmail.com>
2024-03-11 15:09:38 -0300

    chore: add migration create table user

---
+++ src/main/resources/db/migration/V1__create-table-user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_user (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  accessLevel VARCHAR(255)
+);
