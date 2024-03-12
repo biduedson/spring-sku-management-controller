cfb8b542a12cb3a8c3528051c3b4fb60075ab1de
Edson <biduzao.bidu21@gmail.com>
2024-03-11 23:08:17 -0300

    feat: add user view interface

---
+++ src/main/java/com/example/sku_manager/application/interfaces/UserView.java
@@ -0,0 +1,12 @@
+package com.example.sku_manager.application.interfaces;
+
+
+import java.util.Date;
+
+public interface UserView {
+
+    Integer getId();
+    String getUsername();
+    String getEmail();
+    String getAccesslevel();
+}
