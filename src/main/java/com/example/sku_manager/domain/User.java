69e2905799c49401d857f81dc76a8bddc594ec77
Edson <biduzao.bidu21@gmail.com>
2024-03-13 16:14:11 -0300

    refact: recreate user

---
+++ src/main/java/com/example/sku_manager/domain/User.java
@@ -0,0 +1,34 @@
+package com.example.sku_manager.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@Entity
+@Table(name = "tb_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    @Column(name = "accesslevel")
+    private String accesslevel;
+
+
+    public User(String username, String email, String password, String accesslevel) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.accesslevel = accesslevel;
+    }
+
+
+}
