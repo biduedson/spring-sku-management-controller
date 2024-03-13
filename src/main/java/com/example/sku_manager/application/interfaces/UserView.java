8fff32d02faab169ccd0c36879fdb824a023081b
Edson <biduzao.bidu21@gmail.com>
2024-03-13 16:29:33 -0300

    feat: create UserView

--- src/main/java/com/example/sku_manager/application/interfaces/UserView.java
+++ src/main/java/com/example/sku_manager/application/interfaces/UserView.java
@@ -4,9 +4,11 @@ package com.example.sku_manager.application.interfaces;
 import java.util.Date;
 
 public interface UserView {
-
-    Integer getId();
     String getUsername();
     String getEmail();
     String getAccesslevel();
+    Integer getId();
+
+
+
 }
