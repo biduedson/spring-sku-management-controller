6491af5b6adfc1349ebe339fc1ad6faa49ab8599
Edson <biduzao.bidu21@gmail.com>
2024-03-13 16:28:06 -0300

    chore: add news methods

--- src/main/java/com/example/sku_manager/infrastructure/database/UserRepositoryDB.java
+++ src/main/java/com/example/sku_manager/infrastructure/database/UserRepositoryDB.java
@@ -1,9 +1,15 @@
 package com.example.sku_manager.infrastructure.database;
 
-import com.example.sku_manager.domain.user.User;
+import com.example.sku_manager.application.interfaces.UserView;
+import com.example.sku_manager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepositoryDB  extends JpaRepository<User, Integer> {
+    List<UserView> findAllProjectedByOrderByIdAsc();
+
+    UserView  findProjectedById(Integer id);
     User findByUsername(String name);
     User findByEmail(String email);
 }
