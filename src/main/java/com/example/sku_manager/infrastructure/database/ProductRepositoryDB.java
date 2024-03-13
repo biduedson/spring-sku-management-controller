76878e5e92e4baa276a982875dc6a0abd00320b5
Edson <biduzao.bidu21@gmail.com>
2024-03-13 16:25:24 -0300

    feat: create product repository

--- src/main/java/com/example/sku_manager/infrastructure/database/ProductRepositoryDB.java
+++ src/main/java/com/example/sku_manager/infrastructure/database/ProductRepositoryDB.java
@@ -1,7 +1,13 @@
 package com.example.sku_manager.infrastructure.database;
 
-import com.example.sku_manager.domain.product.Product;
+import com.example.sku_manager.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepositoryDB extends JpaRepository<Product, Integer> {
+    Product findByName(String name);
+    Product findBySku(String sku);
+    Product findByImgurl(String imgurl);
+    Product findByGtin(String gtin);
+
+
 }
