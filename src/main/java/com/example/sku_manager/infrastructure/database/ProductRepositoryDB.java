0db6610f8a3c4737c3282aca492bc11f540ee3bc
Edson <biduzao.bidu21@gmail.com>
2024-03-19 20:21:11 -0300

    chore: added new ways to search the database

--- src/main/java/com/example/sku_manager/infrastructure/database/ProductRepositoryDB.java
+++ src/main/java/com/example/sku_manager/infrastructure/database/ProductRepositoryDB.java
@@ -4,10 +4,15 @@ import com.example.sku_manager.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepositoryDB extends JpaRepository<Product, Integer> {
+
     Product findByName(String name);
     Product findBySku(String sku);
     Product findByImgurl(String imgurl);
     Product findByGtin(String gtin);
+    boolean existsByName(String name);
+    boolean existsBySku(String sku);
+    boolean existsByImgurl(String imgurl);
+    boolean existsByGtin(String gtin);
 
 
 }
