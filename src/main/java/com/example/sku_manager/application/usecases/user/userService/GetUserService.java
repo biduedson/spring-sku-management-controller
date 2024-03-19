a2e00d98e1afae0bbad767da3c2b1f0f37a7cfbc
Edson <biduzao.bidu21@gmail.com>
2024-03-19 20:26:13 -0300

    chore:delete java.utils

--- src/main/java/com/example/sku_manager/application/usecases/user/userService/GetUserService.java
+++ src/main/java/com/example/sku_manager/application/usecases/user/userService/GetUserService.java
@@ -1,9 +1,6 @@
 package com.example.sku_manager.application.usecases.user.userService;
 
-import com.example.sku_manager.application.interfaces.UserView;
 import com.example.sku_manager.domain.HttpResponses;
-
-import java.util.List;
 
 public interface GetUserService {
    HttpResponses  getAllUsers();
